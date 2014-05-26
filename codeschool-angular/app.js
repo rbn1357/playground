c94d954805ca4f05ec56c9a0981478fd225a8a0d
Ryan Wood <rbn1357@gmail.com>
2014-05-25 17:22:43 -0700

    added store controller and gem

--- codeschool-angular/app.js
+++ codeschool-angular/app.js
@@ -1,6 +1,10 @@
 (function() {
   var app = angular.module('store', []);
 
+  app.controller('StoreController', function(){
+    this.product = gem
+  });
+
   var gem = {
     name: 'Dodecahedron',
     price: 2.95,
