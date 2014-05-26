692f06815bb2bf27bb3944cb102657656b7165d2
Ryan Wood <rbn1357@gmail.com>
2014-05-26 16:56:12 -0700

    dynamically add review to product's reviews

--- codeschool-angular/app.js
+++ codeschool-angular/app.js
@@ -13,12 +13,16 @@
     };
 
     this.isSelected = function(checkTab){
-      return this.tab === checkTab
+      return this.tab === checkTab;
     };
   });
 
   app.controller('ReviewController', function(){
     this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+    };
   });
 
   var gems = [
