bc5eaf4f8c114feffe6ab099db14f561b2415c1f
Ryan Wood <rbn1357@gmail.com>
2014-05-25 17:21:28 -0700

    added a gem

--- codeschool-angular/app.js
+++ codeschool-angular/app.js
@@ -1,3 +1,9 @@
 (function() {
-  var app = angular.module('store', [])
+  var app = angular.module('store', []);
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'Coolest Gem Evar'
+  };
 })();
