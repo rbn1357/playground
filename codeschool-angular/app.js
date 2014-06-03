b9a7de9b6bd3e48c85c456d1882ddde55969b465
Ryan Wood <rbn1357@gmail.com>
2014-06-02 19:56:06 -0700

    made custom directive to support product info template
    * Defined directive in js
    * Moved h3 tag into template

--- codeschool-angular/app.js
+++ codeschool-angular/app.js
@@ -1,6 +1,13 @@
 (function() {
   var app = angular.module('store', []);
 
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = gems;
   });
