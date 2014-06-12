6c256e19fa730b784e8824a436b48c91d27d6425
Ryan Wood <rbn1357@gmail.com>
2014-06-12 15:09:38 -0700

    refactored: moved review into sep module

--- codeschool-angular/app.js
+++ codeschool-angular/app.js
@@ -1,17 +1,8 @@
 (function() {
-  var app = angular.module('store', ['store-products']);
+  var app = angular.module('store', ['store-products', 'store-reviews']);
 
   app.controller('StoreController', function(){
     this.products = gems;
-  });
-
-  app.controller('ReviewController', function(){
-    this.review = {};
-
-    this.addReview = function(product){
-      product.reviews.push(this.review);
-      this.review = {};
-    };
   });
 
   var gems = [
---
+++ codeschool-angular/review.js
@@ -0,0 +1,13 @@
+(function(){
+  var app = angular.module('store-reviews', []);
+
+  app.controller('ReviewController', function(){
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+  
+})();
